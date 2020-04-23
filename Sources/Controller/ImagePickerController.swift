3ce4c4134cc78e46ddc43ba1ca075152e9843e51
Trương Văn Quang <47170201+truongquang1993@users.noreply.github.com>
2020-04-23 23:29:50 +0700

    Update ImagePickerController.swift

--- Sources/Controller/ImagePickerController.swift
+++ Sources/Controller/ImagePickerController.swift
@@ -35,7 +35,7 @@ fileprivate var localizedCancel = "Cancel"
     public weak var imagePickerDelegate: ImagePickerControllerDelegate?
     public var settings: Settings = Settings()
     public var doneButton: UIBarButtonItem = UIBarButtonItem(title: localizedDone, style: .done, target: nil, action: nil)
-    public var cancelButton: UIBarButtonItem = UIBarButtonItem(title: localizedCancel, style: .cancel, target: nil, action: nil)
+    public var cancelButton: UIBarButtonItem = UIBarButtonItem(title: localizedCancel, style: .plain, target: nil, action: nil)
     public var albumButton: UIButton = UIButton(type: .custom)
     public var selectedAssets: [PHAsset] {
         get {
